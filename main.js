d6ccd5d54eaf4362cc118149662eabcecdc74ab6
Matheus-S-Costa <matheusilva334@gmail.com>
2023-08-03 17:31:56 -0300

    add animation commit

--- main.js
+++ main.js
@@ -1,30 +1,13 @@
-$('.navbar a').on('click', function (e) {
-    if (this.hash !== '') {
-      e.preventDefault();
- 
-      const hash = this.hash;
- 
-      $('html, body')
-        .animate({
-          scrollTop: $(hash).offset().top
-        },800);
+const myObserver = new IntersectionObserver( (entries) => {
+  entries.forEach( (entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
     }
-  });
+  })
+})
 
-  var modal = document.querySelector(".modal");
-  var trigger = document.querySelector(".trigger");
-  var closeButton = document.querySelector(".close-button");
-  
-  function toggleModal() {
-      modal.classList.toggle("show-modal");
-  }
-  
-  function windowOnClick(event) {
-      if (event.target === modal) {
-          toggleModal();
-      }
-  }
-  
-  trigger.addEventListener("click", toggleModal);
-  closeButton.addEventListener("click", toggleModal);
-  window.addEventListener("click", windowOnClick);
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach( (element) => myObserver.observe(element) )
