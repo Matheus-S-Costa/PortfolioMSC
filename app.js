5f24061c827379a5b072a2a226da01aea5f3b431
Matheus-S-Costa <matheusilva334@gmail.com>
2023-08-05 15:02:19 -0300

    dark mode commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const inputContainer = document.querySelector('.input')
+const rootElement = document.documentElement
+
+const lightTheme = {
+    '--background-color': '#ededed',
+    '--primary-color': '#f1f1f1',
+    '--secondary-color': '#f0f0f0',
+    '--menu-color': '#b4b4b4',
+    '--white-color': '#111111',
+    '--light-gray-color': 'black',
+}
+const darkTheme = {
+    '--background-color': '#121214',
+    '--primary-color': '#0A1128',
+    '--secondary-color': '#1f1f23',
+    '--menu-color': '#0a0a0b',
+    '--light-gray-color': 'silver',
+    '--white-color': '#ffffff',
+}
+
+inputContainer.addEventListener('change', function() {
+  const isChecked = inputContainer.checked
+  if (isChecked) {
+    changeTheme(lightTheme)
+  } else {
+    changeTheme(darkTheme)
+  }
+
+})
+
+function changeTheme(theme) {
+    for (let prop in theme){
+        changeProperty(prop, theme[prop])
+    }
+}
+
+function changeProperty(property, value) {
+    rootElement.style.setProperty(property, value)
+}
